43b01c392ff64e3996a7b401990461c8a114f344
Grzegorz Błaszczuk <grzegorz.blaszczuk36@gmail.com>
2024-12-04 21:12:40 +0100

    automatic refresh players

--- src/queries/useCreatePlayerMutation.ts
+++ src/queries/useCreatePlayerMutation.ts
@@ -1,13 +1,19 @@
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { useApi } from "../hooks/useApi"
 import { PlayerDto, PlayerEntity } from "../types";
 
 export const useCreatePlayerMutation = () =>  {
+    const queryClient = useQueryClient()
     const { apiPost } = useApi();
     const { mutate, isPending} = useMutation({
         mutationKey: ['players', 'create'],
         mutationFn: async(payload: PlayerDto) => {
             return apiPost<PlayerEntity, PlayerDto>('players', payload)
+        },
+        onSuccess: () => {
+            queryClient.invalidateQueries({
+                queryKey: ['players']
+            })
         }
 })
 return {
