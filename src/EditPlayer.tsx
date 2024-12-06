22a92284c6e8d2d8c6e778608fea755242821c45
Grzegorz Błaszczuk <grzegorz.blaszczuk36@gmail.com>
2024-12-06 19:24:37 +0100

    Create EditPlayer and modification SinglePlayer

---
+++ src/EditPlayer.tsx
@@ -0,0 +1,59 @@
+import { ChangeEvent, FormEvent, useState } from "react"
+import { useUpdatePlayerMutation } from "./queries/useUpdatePlayerMutation"
+import { PlayerEntity } from "./types"
+
+type EditPlayerProps = {
+    player: PlayerEntity
+    showEdit(): void
+}
+
+export const EditPlayer = ({player, showEdit}: EditPlayerProps) => {
+    const {isPending, error, mutate: EditPlayer} = useUpdatePlayerMutation(player.id)
+
+    const [values, setValues] = useState({
+        name: player.name,
+        lastname: player.lastname,
+        teamId: player.teamId,
+    })
+
+    const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+        const { name, value } = e.target;
+        setValues(prevValues =>  ({
+            ...prevValues, [name]: value
+        }))
+    }
+
+    const handleCancle = () => {
+        showEdit()
+    }
+
+    const handleSubmit = (e: FormEvent) => {
+        e.preventDefault()
+
+        EditPlayer({
+            name: values.name,
+            lastname: values.lastname,
+            teamId: values.teamId
+        })
+
+    }
+
+    if(isPending) return <p>Loading...</p>
+    if(error) return <p>{error.message}</p>
+
+    return (
+        <form onSubmit={handleSubmit}>
+        <div>
+            <label htmlFor="name">Name</label>
+            <input type='text' name='name' id='name' value={values.name} onChange={handleChange}/>
+        </div>
+        <div>
+            <label htmlFor="lastname">LastName</label>
+            <input type='text' name='lastname' id='lastname' value={values.lastname} onChange={handleChange}/>
+        </div>
+        <button type='submit' disabled={isPending}>Edit Player</button>
+        <button onClick={handleCancle} disabled={isPending}>Cancle</button>
+    </form>
+    )
+
+}
--- src/SinglePlayer.tsx
+++ src/SinglePlayer.tsx
@@ -1,6 +1,7 @@
 import { useState } from "react"
 import { DeletePlayerConfirmation } from "./DeletePlayerConfirmation"
-import { PlayerEntity } from "./types"
+import { PlayerEntity} from "./types"
+import { EditPlayer } from "./EditPlayer"
 
 type SinglePlayerProps =  {
     player: PlayerEntity
@@ -8,9 +9,15 @@ type SinglePlayerProps =  {
 
 export const SinglePlayer = ({player}: SinglePlayerProps) => {
     const [isDeleted, setIsDeleted] = useState(false)
+    const [isEdited, setIsEditted] = useState(false)
 
     const handleDeleted = () => {
         setIsDeleted(!isDeleted)
+    }
+
+    const handleEdit = () => {
+        setIsEditted(!isEdited)
+
     }
 
     const {id, name, lastname, teamName, teamId} = player
@@ -21,8 +28,10 @@ export const SinglePlayer = ({player}: SinglePlayerProps) => {
             <h2><strong>{name} {lastname}  - {teamName || "Free" }</strong></h2>
             <p>
                 <button onClick={handleDeleted}>Delete</button>
+                <button onClick={handleEdit}>Edit</button>
             </p>
             {isDeleted ? <DeletePlayerConfirmation id={id} showDelete={handleDeleted} idTeam={teamId}/> : null}
+            {isEdited ? <EditPlayer player={player} showEdit={handleEdit} /> : null}
         </li>
         </>
     )
