0e99688603783ee7536bde3023646f79ce580625
Szymon <edbsiedlce@gmail.com>
2025-01-01 14:25:31 +0100

    Modification useDeleteTeamMutation for remove error with freedom players after deleted team

--- src/queries/useDeleteTeamMutation.ts
+++ src/queries/useDeleteTeamMutation.ts
@@ -1,19 +1,27 @@
 import { useMutation, useQueryClient } from "@tanstack/react-query"
 import { useApi } from "../hooks/useApi"
+import { PlayerEntity } from "../types";
 
 export const useDeleteTeamMutation = () => {
-    const {apiDelete} = useApi();
+    const {apiDelete, apiPut, apiGet} = useApi();
     const queryClient = useQueryClient()
     
     const { mutate, isPending, error} = useMutation({
         mutationKey: ['teams'],
         mutationFn: async(id: string) => {
+            const players: PlayerEntity[] = await apiGet('players');
+            const updatePlayersAfterDeleteTeam = players
+            .filter((player: PlayerEntity) => player.teamId === id)
+            .map((player: PlayerEntity) => apiPut(`players/${player.id}`, { teamId: ""}));
+            await Promise.all(updatePlayersAfterDeleteTeam)
+
+
+
             return apiDelete(`teams/${id}`)
         },
         onSuccess: () => {
-            queryClient.invalidateQueries({
-                queryKey: ['teams']
-            })
+            queryClient.invalidateQueries({ queryKey: ['teams'] });
+            queryClient.invalidateQueries({ queryKey: ['players'] })
         }
     })
 
